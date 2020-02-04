d3349892745506dff0289fffb29c68ef5765206e
Jodi Pranata <jodranata@gmail.com>
2020-02-04 23:13:34 +0700

    build initial commit

---
+++ build/precache-manifest.063defe66e0aabc992e7163f319cd3c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33be1ed7f12a25520c43a214fa35bf9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "379ac1c6c3e586601ab2",
+    "url": "/static/css/main.d7067ed2.chunk.css"
+  },
+  {
+    "revision": "e839dee010495e705345",
+    "url": "/static/js/2.19de9453.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/static/js/2.19de9453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379ac1c6c3e586601ab2",
+    "url": "/static/js/main.f1bc87be.chunk.js"
+  },
+  {
+    "revision": "d47e746056be10ce9609",
+    "url": "/static/js/runtime-main.8c13799a.js"
+  },
+  {
+    "revision": "a77a9fc14733c78d00746a077a874ce5",
+    "url": "/static/media/blank.a77a9fc1.png"
+  },
+  {
+    "revision": "a9f2094bf0480d0234879594b7d699af",
+    "url": "/static/media/howlersiconwide.a9f2094b.png"
+  }
+]);
